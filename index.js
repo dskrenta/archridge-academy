f4b6bad7c6e75d6e9c3dd0ad181b95a5655fee1f
dskrenta <dskrenta@gmail.com>
2017-05-17 03:39:48 -0700

    reordered game strucutre

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 'use strict';
 const prompt = require('prompt');
 
-prompt.start();
+class ArchridgeAcademy {
+  constructor() {
+    this.playerName = 'David';
+    prompt.start();
+    this.gamePrompt();
+  }
 
-/*
-prompt.get('playername', (err, result) => {
-  console.log(`Entered: ${result.playername}`);
-});
-*/
+  gamePrompt() {
+    return prompt.get(this.playerName, (error, result) => {
+      this.gameLogic(result[this.playerName]);
+    });
+  }
 
-mainPrompt('Hello');
-
-function mainPrompt(input) {
-  return prompt.get(input, (err, result) => {
-    console.log(`Entered: ${result[input]}`);
-  });
+  gameLogic(input) {
+    if (input.match(/move/)) {
+      console.log('move!');
+    } else if (input.match(/help/)) {
+      console.log('help!');
+    } else if (input.match(/quit/)) {
+      console.log('Goodbye!');
+      process.exit();
+    } else {
+      console.log('Invalid input');
+    }
+    this.gamePrompt();
+  }
 }
+
+const game = new ArchridgeAcademy();
